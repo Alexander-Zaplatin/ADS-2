a0d71350bc56d17ad1af7bb65b49e5bb82eb12ac
Alexander-Zaplatin <79161134+Alexander-Zaplatin@users.noreply.github.com>
2021-04-07 18:50:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,7 @@ int countPairs1(int *arr, int len, int value) {
 int res = 0;
 for (int i = 0; i < len - 1; i++) {
 for (int j = i + 1; j < len; j++) {
-if (arr[i] + arr[j] == value) res++;
+if ((arr[i] + arr[j]) == value) res++;
 }
 }
   return res;
@@ -12,16 +12,19 @@ if (arr[i] + arr[j] == value) res++;
 
 int countPairs2(int *arr, int len, int value) {
 int res = 0;
-int num = 0;
-int m = len - 1;
-int h = -1;
-while (h != value) {
-if (arr[m] > value) m--;
-if (arr[m] == value) h = value;
+int l = 0;
+int r = len - 1;
+while ((r - 1) > l) {
+int mid = (r + l) / 2;
+if (value >= arr[mid]) l = mid;
+else
+r = mid;
 }
-for (int i = 0; i < m - 1; i++) {
-for (int j = 0; i < m; j++) {
-if (arr[i] + arr[j] == value) res++;
+len = r - 1;
+for (int i = len; i >= 0; i--) {
+for (int j = 0; j < i; j++) {
+if ((arr[i] + arr[j]) == value) res++;
+if ((arr[i] + arr[j]) > value) break;
 }
 }
   return res;
